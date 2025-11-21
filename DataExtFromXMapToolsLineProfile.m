a4e9129055c0bb7664292c6f4eafac630a5d5b8d
福島諒 <rpifukushima@gmail.com>
2025-11-21 15:23:50 +0900

    description added

--- DataExtFromXMapToolsLineProfile.m
+++ DataExtFromXMapToolsLineProfile.m
@@ -1,9 +1,11 @@
 %
 % This is an Octave script for extracting x/y data from a .fig file
-% obtained with a line-profile generator in XMapTools.
+% obtained with the line-profile generator in XMapTools.
+%
+% Tested with XMapTools v4.2/4.3 and GNU Octave v7.3.0/v10.3.0.
 %
 % Author: Ryo Fukushima (Kochi JAMSTEC)
-% ver. Nov-20-2025
+% ver. Nov-21-2025
 %
 clear,close all
 [FileName, PathName, FilterIndex] = uigetfile({'*.fig'},'File Selector');
